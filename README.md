275472f48259af0c3fa3a8c0a46e8670b8025675
plus <zxwcfy@gmail.com>
2016-02-05 14:12:32 +0200

    main

---
+++ README.md
@@ -0,0 +1,23 @@
+# smart-mode-line-powerline2
+an alternate powerline theme for smart-mode-line (emacs)
+
+![demo](demo.gif)
+
+## Installation
+Place `smart-mode-line-powerline2-theme.el` in your theme load path (defined by variable `custom-theme-load-path`).
+Append the following to your emacs init file (~/.emacs by default). `sml/theme` should be set before `(sml/setup)` is called.
+```
+(setq sml/theme 'powerline2)
+(sml/setup)
+```
+
+## Customization
+`mode-line-inactive` — inactive modeline and outer layer of active modeline.
+`powerline-active1` — middle layer of active modeline.
+`powerline-active2` — inner layer of active modeline.
+Additionally you should have a look at the Smart Mode Line Faces customization group (`M-x customize-group smart-mode-line-faces`).
+
+You might want to decrease the minimum of `sml/name-width` to get the inner layer text to display nearer to the separator.
+
+Here is what it looks like with the default emacs theme:
+![default](default.png)
---
+++ smart-mode-line-powerline2-theme.el
@@ -0,0 +1,114 @@
+;;; smart-mode-line-powerline2-theme.el --- an alternate powerline theme for smart-mode-line
+
+;; Original powerline theme and smart-mode-line are by Artur Malabarba
+;; http://github.com/Bruce-Connor/smart-mode-line
+;; maintained by plu5 http://github.com/plu5/smart-mode-line-powerline2
+;; Package-Requires: ((emacs "24.3") (powerline "2.2") (smart-mode-line "2.5"))
+
+;;; License:
+;;
+;; This file is NOT part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 2
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+
+;;; Code:
+
+(deftheme smart-mode-line-powerline2
+	"an alternate powerline theme for smart-mode-line")
+(require 'powerline)
+
+(set-face-attribute 'powerline-active2 nil :inherit 'sml/global)
+(set-face-attribute 'powerline-active1 nil :inherit 'sml/global)
+(let ((l0 (face-background 'mode-line-inactive))
+      (l3 (or (face-background 'powerline-active1) "grey22"))
+      (l8 (or (face-background 'powerline-active2) "grey40"))
+      (separator-left
+       '(intern (format "powerline-%s-%s"
+                        (powerline-current-separator)
+                        (car powerline-default-separator-dir))))
+      (separator-right
+       '(intern (format "powerline-%s-%s"
+                        (powerline-current-separator)
+                        (cdr powerline-default-separator-dir)))))
+  (custom-theme-set-faces
+   'smart-mode-line-powerline2
+   `(mode-line-buffer-id ((t :inherit sml/filename :foreground nil :background nil)))
+   `(mode-line-inactive ((t :box (:line-width -8 :color ,l0))))
+   `(mode-line ((t :background ,l0)))
+   `(sml/global    ((t :foreground "grey50" :inverse-video nil)))
+
+   ;; Layer 0
+   `(sml/line-number         ((t :foreground "grey10" :inherit sml/global :weight bold :background ,l0)))
+   `(sml/remote              ((t :inherit sml/global :background ,l0)))
+   `(sml/col-number          ((t :inherit sml/global :foreground "grey20" :background ,l0)))
+   `(sml/numbers-separator   ((t :inherit sml/col-number :background ,l0)))
+   `(sml/client              ((t :inherit sml/prefix :background ,l0)))
+   `(sml/mule-info           ((t :inherit sml/global :background ,l0)))
+   `(sml/not-modified        ((t :inherit sml/global :background ,l0)))
+   '(sml/read-only           ((t :inherit sml/not-modified :foreground "Cyan")))
+
+   ;; 3
+   `(sml/prefix    ((t :background ,l3 :inherit sml/global :foreground "#bf6000")))
+   `(sml/filename  ((t :background ,l3 :inherit sml/global :foreground "black")))
+   `(sml/sudo      ((t :background ,l3 :inherit sml/outside-modified)))
+   `(sml/git       ((t :background ,l3 :inherit (sml/read-only sml/prefix))))
+   `(sml/folder    ((t :background ,l3 :inherit sml/global :weight normal :foreground "Black")))
+
+   ;; 8
+   `(sml/name-filling        ((t :background ,l8 :inherit sml/prefix :weight normal)))
+   `(sml/position-percentage ((t :background ,l8 :inherit sml/prefix :weight normal :foreground "#330000")))
+   `(sml/modes               ((t :background ,l8 :inherit sml/global :foreground "Black")))
+   `(sml/process             ((t :background ,l8 :inherit sml/prefix)))
+   `(sml/vc                  ((t :background ,l8 :inherit sml/git :foreground "#0000aa")))
+   `(sml/vc-edited           ((t :background ,l8 :inherit sml/prefix :foreground "#330000")))
+
+   ;; 3
+   ;; minor modes
+   `(sml/minor-modes         ((t :inherit sml/folder)))
+
+   ;; 0
+   `(sml/discharging         ((t :background ,l0 :inherit sml/global :foreground "Red")))
+   `(sml/time                ((t :background ,l0 :inherit sml/global)))
+
+   `(persp-selected-face ((t :foreground "ForestGreen" :inherit sml/filename)))
+   `(helm-candidate-number ((t :foreground nil :background nil :inherit sml/filename))))
+  (custom-theme-set-variables
+   'smart-mode-line-powerline2
+   '(sml/mode-width (if (eq (powerline-current-separator) 'arrow) 'right 'full))
+   `(sml/pre-id-separator
+     '(""
+       (:propertize " " face sml/global)
+       (:eval (propertize " " 'display (funcall ,separator-left nil 'powerline-active1)))
+       (:propertize " " face powerline-active1)))
+   `(sml/pos-id-separator
+     '(""
+       (:propertize " " face powerline-active1)
+       (:eval (propertize " " 'display (funcall ,separator-left 'powerline-active1 'powerline-active2)))
+       (:propertize " " face powerline-active2)))
+   `(sml/pre-minor-modes-separator
+     '("" (:propertize " " face powerline-active2)
+       (:eval (propertize " " 'display (funcall ,separator-right 'powerline-active2 'powerline-active1)))
+       (:propertize " " face powerline-active1)))
+   `(sml/pos-minor-modes-separator
+     '("" (:propertize " " face powerline-active1)
+       (:eval (propertize " " 'display (funcall ,separator-right 'powerline-active1 nil)))
+       (:propertize " " face sml/global)))
+   '(sml/pre-modes-separator
+     (propertize " " 'face 'sml/modes))))
+
+;;;###autoload
+(when load-file-name
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
+
+(provide-theme 'smart-mode-line-powerline2)
+;;; smart-mode-line-powerline2-theme.el ends here
